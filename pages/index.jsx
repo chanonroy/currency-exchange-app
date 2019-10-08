9ca5074858c5b85ff87d55eda015adf1cbdd2cd4
Chanon Roy <chanonroy@gmail.com>
2019-10-08 21:29:16 +0100

    chore: add eslint

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Item from "../components/Item";
+import Head from "../components/Head";
+
+const App = () => {
+  return (
+    <>
+      <Head title="Currency Exchange App" />
+      <Item />
+    </>
+  );
+};
+
+export default App;
